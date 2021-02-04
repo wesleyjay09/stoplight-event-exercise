9321d073a880cd8e0465f3e5de961399c1a95e99
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-02-04 14:23:31 -0600

    Stop Light

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-(function() {
-  'use strict';
+var stopButton = document.querySelector('#stopButton');
+var slowButton = document.querySelector('#slowButton');
+var goButton = document.querySelector('#goButton');
+var stopLight = document.querySelector('#stopLight');
+var slowLight = document.querySelector('#slowLight');
+var goLigth = document.querySelector('#goLight');
 
-  // YOUR CODE HERE
-})();
+stopButton.addEventListener('click', function(){
+  if(!stopLight.classList.contains('stop')) {
+    stopLight.classList.add('stop')
+  } else if (stopLight.classList.contains('stop')) {
+      stopLight.classList.remove('stop')
+  }
+});
+slowButton.addEventListener('click', function(){
+  if(!slowLight.classList.contains('slow')) {
+    slowLight.classList.add('slow')
+  } else if (slowLight.classList.contains('slow')) {
+      slowLight.classList.remove('slow')
+  }
+});
+goButton.addEventListener('click', function(){
+  if(!goLight.classList.contains('go')) {
+    goLight.classList.add('go')
+  } else if (goLight.classList.contains('go')) {
+      goLight.classList.remove('go')
+  }
+});
+
+
+
